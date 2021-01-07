1638dbacde8c13d5d2913e202caaa74cc485a0ea
Iris <royalvald@sina.com>
2021-01-07 22:49:21 +0800

    add file node and dir node

---
+++ FastFileSystem/cmake-build-debug/CMakeFiles/FastFileSystem.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FastFileSystem.dir/main.cpp.obj"
+  "FastFileSystem.exe"
+  "FastFileSystem.exe.manifest"
+  "FastFileSystem.pdb"
+  "libFastFileSystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FastFileSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ FastFileSystem/cmake-build-debug/CMakeFiles/FastFileSystem.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for FastFileSystem.
+# This may be replaced when dependencies are built.
