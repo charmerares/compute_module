e5955c336bcbb6b45a9b1e5e2f3d4a6b3d3cfef8
iris <royalvald@sina.com>
2021-04-20 22:06:13 +0800

    add compute util
    [修改说明】
    1.添加部分函数签名

---
+++ FastFileSystem/os/fast/include/compute_util.h
@@ -0,0 +1,39 @@
+//
+// Created by iris on 2021/4/20.
+//
+
+#ifndef FASTFILESYSTEM_COMPUTE_UTIL_H
+#define FASTFILESYSTEM_COMPUTE_UTIL_H
+
+#include <list>
+
+using namespace std;
+
+/**
+ * 计算方差
+ * @param list
+ * @return
+ */
+double compute_variance(list<double> list);
+
+/**
+ * 计算标准差
+ * @param list
+ * @return
+ */
+double compute_standard_deviation(list<double> list);
+
+/**
+ * 计算极差
+ * @param list
+ * @return
+ */
+double compute_extreme_difference(list<double> list);
+
+/**
+ * 求和
+ * @param list
+ * @return
+ */
+double compute_sum(list<double> list);
+#endif //FASTFILESYSTEM_COMPUTE_UTIL_H
